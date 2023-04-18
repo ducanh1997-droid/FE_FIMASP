fe04f2d6a83a425890dba5aa5f9011b261b417b4
Linh123321 <linhtrantb04@gmail.com>
2023-04-18 14:07:52 +0700

    Wallet_CRUD

--- src/pages/Home/Wallet.js
+++ src/pages/Home/Wallet.js
@@ -1,35 +1,45 @@
-export default function Wallet(){
-    return(
-        <div id="content-account">
-            <h2 id="page-title-account">Danh sách tài khoản</h2>
-            <hr id="hr-search-account"/>
-            <div style={{display: "inline-block", marginTop: 15}}>
-                <h1 id="page-title-list-account" style={{float: "left"}}>
-                    Thêm tài khoản
-                </h1>
-                <div
-                    className="icon-border-dashboard"
-                    style={{cursor: "pointer"}}
-                    // onClick="createFormAddAccount()"
-                >
-                    <i className="fa-solid fa-plus"/>
-                </div>
-            </div>
-            <hr id="hr-list-account"/>
-            <table id="table-list-account">
-                <tbody>
-                <tr>
-                    <th>Danh mục</th>
-                    <th>Số tiền</th>
-                    <th>Tên tài khoản</th>
-                    <th>Hạn sử dụng</th>
-                    <th>Sửa</th>
-                    <th>Xoá</th>
-                </tr>
-                </tbody>
-                <tbody id="data-account"></tbody>
-            </table>
-        </div>
+import {useState} from "react";
+import axios from "axios";
+import AWalletElement from "./AWalletElement";
+import WalletDetailContent from "./WalletDetailContent";
+import CreateWalletForm from "./createWalletForm";
 
+import("./Wallet.css")
+export default function Wallet(){
+    const [wallets,setWallets]=useState([])
+    const [walletChoice,setWalletChoice]=useState(null)
+    const [isUpdate,setIsUpdate]=useState(true)
+    const [click,setClick]=useState(false)
+    const [showCreateForm,setShow]=useState(false)
+    if(isUpdate){
+        axios.get("http://localhost:3000/wallets").then((res)=>{
+            setWallets(res.data)
+            setIsUpdate(false)
+            setWalletChoice(res.data[0])
+        })
+    }
+    return(<>
+        <div className="content-account wallet-content-account" style={!showCreateForm?{filter: "blur(0px)"}:{filter: "blur(3px)"}}>
+            <div
+                className="icon-border-wallet"
+                style={{cursor: "pointer"}}
+                onClick={()=>{setShow(true)}}
+            >
+                <i className="fa-solid fa-plus"/>
+            </div>
+            <div className={"wallet-container"}>
+                {wallets.map(wallet=>{
+                    return <AWalletElement key={wallet.id} setClick={setClick} wallet={wallet} setWalletChoice={setWalletChoice}></AWalletElement>
+                })
+                }
+            </div>
+            <div className={"wallet-detail"}>
+                <WalletDetailContent wallet={walletChoice} click={click} setClick={setClick} setWalletChoice={setWalletChoice} setIsUpdate={setIsUpdate}></WalletDetailContent>
+            </div>
+            <div className={"wallet-statistics"}></div>
+            <div className={"wallet-chart"}></div>
+        </div>
+            {showCreateForm&&<CreateWalletForm setIsUpdate={setIsUpdate} setShow={setShow}></CreateWalletForm>}
+        </>
     )
 }
---
+++ src/pages/Home/createWalletForm.js
@@ -0,0 +1,124 @@
+import {Field, Form, Formik} from "formik";
+import {useState} from "react";
+import axios from "axios";
+import("./CreateWalletForm.css")
+
+export default function CreateWalletForm({setShow,setIsUpdate}){
+    const [activeIcon,setActiveIcon] = useState("")
+    const [activeColor,setActiveColor] = useState("")
+    function setIcon(e){
+        setActiveIcon(e.currentTarget.id);
+    }
+    function setColor(e){
+        setActiveColor(e.currentTarget.style.backgroundImage)
+    }
+    function save(values){
+        let wallet={
+            id: values.id,
+            name: values.name,
+            totalMoney: values.totalMoney,
+            icon: activeIcon,
+            backgroundColor: activeColor,
+            limitMoney: values.limitMoney
+        }
+        axios.post("http://localhost:3000/wallets",wallet).then(()=>{
+            setIsUpdate(true)
+            setShow(false)
+        })
+    }
+    return (
+        <div className={"form-create-wallet"}>
+            <Formik
+                initialValues={{
+                    id: "",
+                    name: "",
+                    totalMoney: "",
+                    limitMoney: ""
+                }}
+                onSubmit={(values) => {
+                    if(activeIcon!==""&&activeColor!==""){
+                        save(values)
+                    }
+                }}
+            >
+                {({values})=><Form>
+                    <table className={"walletDetail-content"}>
+                        <thead></thead>
+                        <tbody>
+                        <tr>
+                            <td><p>Name:</p></td>
+                            <td><Field name={"name"}></Field></td>
+                        </tr>
+                        <tr>
+                            <td><p>Total Money:</p></td>
+                            <td><Field name={"totalMoney"}></Field></td>
+                        </tr>
+                        <tr>
+                            <td><p>Icon:</p></td>
+                            <td>
+                                <div className="block-wallet" id="block-fa-dumbbell" >
+                                    <div className="icon-wallet" id="fas fa-credit-card" style={{borderRadius:activeIcon==="fas fa-credit-card"?"2px":"100px"}} onClick={setIcon}>
+                                        <i className="fas fa-credit-card"></i>
+                                    </div>
+                                    <p id="1">Tập thể dục</p>
+                                </div>
+                                <div className="block-wallet" id="block-fa-bus" >
+                                    <div className="icon-wallet" id="fa-light fa-bus" style={{borderRadius:activeIcon==="fa-light fa-bus"?"2px":"100px"}} onClick={setIcon}>
+                                        <i className="fa-light fa-bus"></i>
+                                    </div>
+                                    <p id="2">Di chuyển</p>
+                                </div>
+                                <div  className="block-wallet" id="block-fa-mug-saucer">
+                                    <div className="icon-wallet" style={{borderRadius:activeIcon==="fa-light fa-mug-saucer"?"2px":"100px"}} id="fa-light fa-mug-saucer" onClick={setIcon}>
+                                        <i className="fa-light fa-mug-saucer"></i>
+                                    </div>
+                                    <p id="2">Cafe</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><p>Background:</p></td>
+                            <td>
+                                <div className={"color-sample"} style={{backgroundImage: "linear-gradient(to right bottom, green, black, blue)",opacity:activeColor==="linear-gradient(to right bottom, green, black, blue)"?"1":"0.3"}} onClick={setColor}></div>
+                                <div className={"color-sample"} style={{backgroundImage: "linear-gradient(to right, red, yellow)",opacity:activeColor==="linear-gradient(to right, red, yellow)"?"1":"0.3"}} onClick={setColor}></div>
+                                <div className={"color-sample"} style={{backgroundImage: "linear-gradient(to right, blue, purple)",opacity:activeColor==='linear-gradient(to right, blue, purple)'?"1":"0.3"}} onClick={setColor}></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><p>Limit:</p></td>
+                            <td><Field name={"limitMoney"}></Field></td>
+                        </tr>
+                        <tr>
+                            <td colSpan={2}>
+                                <button type={"submit"}>Confirm</button>
+                                <button type="button"
+                                        onClick={()=>{setShow(false)}}
+                                >Close</button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div className={"sample-wallet-context"}>
+                        <p>Sample wallet</p>
+                        <div className={"wallet"} style={{backgroundImage: activeColor}}>
+                            <p className={"logoName"}>Fimasp</p>
+                            <i className={activeIcon}></i>
+                            <div className={"walletDetail"}>
+                                <p>Wallet 's name:</p>
+                                <p className={"walletName"}>{values?.name}</p>
+                            </div>
+                            <div className={"walletMoney"}>
+                                <p>{values?.totalMoney}</p>
+                            </div>
+                            <div className={"walletLimit"}>
+                                <p>Limit:</p>
+                                <p className={"walletName"}>{values?.limitMoney}</p>
+                            </div>
+                        </div>
+                    </div>
+                </Form>
+            }
+            </Formik>
+        </div>
+    );
+}
